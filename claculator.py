12923912bb38742d92f364241a4d63950adc973f
rajpal sitole <rajpalsitole29@gmail.com>
2026-09-06 16:02:49 +0530

    Add files via upload

---
+++ claculator.py
@@ -0,0 +1,17 @@
+a = int(input("enter your number:"))
+b = int(input("enter your number:"))
+c = input(("enter your operation:"))
+
+if (c == "+"):
+    print(a+b)
+elif(c == "-"):
+    print(a-b)
+elif(c == "*"):
+    print(a*b)
+elif(c =="/"):
+    print(a/b)
+else:
+    print("something is wrong")
+
+
+             
